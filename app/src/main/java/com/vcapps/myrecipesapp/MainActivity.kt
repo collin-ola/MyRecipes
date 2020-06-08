6b39b22ee7667bcdc6f09372e30870000fc0e454
Victoria Aviomoh <aviomohvictoria@gmail.com>
2020-06-08 22:59:22 +0100

    Facebook login function updated to allow firebase authentication

--- app/src/main/java/com/vcapps/myrecipesapp/MainActivity.kt
+++ app/src/main/java/com/vcapps/myrecipesapp/MainActivity.kt
@@ -1,25 +1,20 @@
 package com.vcapps.myrecipesapp
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.facebook.CallbackManager
-import com.facebook.FacebookCallback
-import com.facebook.FacebookException
-import com.facebook.GraphRequest
+import com.facebook.*
 import com.facebook.login.LoginResult
 import com.facebook.login.widget.LoginButton
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -41,39 +36,27 @@ class MainActivity : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
         callbackManager = CallbackManager.Factory.create()
-
+        var email:String?
         val facebookIntent = Intent(this, ProfileActivity::class.java)
         val facebookLoginButton = findViewById<LoginButton>(R.id.facebookLogin)
         facebookLoginButton.setPermissions(listOf("email","public_profile"))
         facebookLoginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
-                override fun onSuccess(loginResult: LoginResult) {
-                    val request = GraphRequest.newMeRequest(loginResult.accessToken) { `object`, _ ->
-                        // Application code
-                        val name = `object`.getString("name")
-                        val email = `object`.getString("email")
-                        val img = `object`.getJSONObject("picture").getJSONObject("data").getString("url")
 
-                        val fbUser = MyRecipeUser(name,email,"","", Uri.parse(img))
-                        fbUser.registerFacebookUser("facebook")
-                    }
+            override fun onSuccess(loginResult: LoginResult) {
+                email = handleFacebookAccessToken(loginResult.accessToken)
+                facebookIntent.putExtra("userEmailAddress", email)
+                startActivity(facebookIntent)
+            }
 
-                    startActivity(facebookIntent)
+            override fun onCancel() {
+                Log.d(TAG, "facebook:onCancel")
+            }
 
-                    val parameters = Bundle()
-                    parameters.putString("fields", "id, name, email, picture.type(large)")
-                    request.parameters = parameters
-                    request.executeAsync()
-                }
+            override fun onError(error: FacebookException?) {
+                Log.d(TAG, "facebook:onError", error)
+            }
 
-                override fun onCancel() {
-                    Log.d(TAG, "facebook:onCancel")
-                }
-
-                override fun onError(error: FacebookException?) {
-                    Log.d(TAG, "facebook:onError", error)
-                }
-
-            })
+        })
 
         if (supportActionBar != null)
             supportActionBar?.hide()
@@ -99,8 +82,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         registerButton.setOnClickListener {
-           val intent = Intent(this, RegistrationActivity::class.java)
-           startActivityForResult(intent, RequestCodes.requestCodeRegister)
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivityForResult(intent, RequestCodes.requestCodeRegister)
         }
 
         forgottenPassword.setOnClickListener {
@@ -109,37 +92,45 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun handleFacebookAccessToken(token: AccessToken?): String? {
+        Log.d(TAG, "handleFacebookAccessToken:$token")
+        val credential = FacebookAuthProvider.getCredential(token!!.token)
+        auth.signInWithCredential(credential)
+
+        return auth.currentUser!!.email
+    }
+
 
 //********************* User Login **********************************
 
-   private fun loginButton(userEmailText: String, userPasswordText: String) {
-       if (userEmailText.trim() == "" || userPasswordText.trim() == "") {
+    private fun loginButton(userEmailText: String, userPasswordText: String) {
+        if (userEmailText.trim() == "" || userPasswordText.trim() == "") {
 
-           makeToast("Please enter your details.")
-           return
-       }
+            makeToast("Please enter your details.")
+            return
+        }
 
-       auth.signInWithEmailAndPassword(userEmailText, userPasswordText)
-           .addOnCompleteListener(this) { task ->
-               if (task.isSuccessful) {
-                   // Sign in success, update UI with the signed-in user's information
-                   Log.d(TAG, "signInWithEmail:success")
-                   makeToast("Logged in. Welcome $userEmailText")
-                   val intent = Intent(this, ProfileActivity::class.java)
-                   intent.putExtra("userEmailAddress", userEmailText)
-                   startActivity(intent)
+        auth.signInWithEmailAndPassword(userEmailText, userPasswordText)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    makeToast("Logged in. Welcome $userEmailText")
+                    val intent = Intent(this, ProfileActivity::class.java)
+                    intent.putExtra("userEmailAddress", userEmailText)
+                    startActivity(intent)
 
-               } else {
-                   // If sign in fails, display a message to the user.
-                   makeToast("Login failed. Try again")
-                   Log.w(TAG, "signInWithEmail:failure", task.exception)
-               }
-           }
-           .addOnFailureListener(this) {
-               makeToast("Error: ${it.message}")
-           }
+                } else {
+                    // If sign in fails, display a message to the user.
+                    makeToast("Login failed. Try again")
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                }
+            }
+            .addOnFailureListener(this) {
+                makeToast("Error: ${it.message}")
+            }
 
-       //******************* User settings ***********************
+        //******************* User settings ***********************
 /*
        val user = FirebaseAuth.getInstance().currentUser
 
@@ -156,12 +147,10 @@ class MainActivity : AppCompatActivity() {
                    startActivity(intent)
                }
            }*/
-   }
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
-        Log.v(TAG, "In OAR...")
 
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RequestCodes.requestCodeSignIn) {
